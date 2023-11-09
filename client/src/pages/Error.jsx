ecc4fede848cbe45ae23276b7513adbedce67318
jannis2301 <jannis.ax@gmx.de>
2023-11-09 22:58:51 +0100

    refactor: error-page styles & icon added

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
 
 const Error = () => {
   return (
-    <ErrorStyles>
-      <div className="">
-        {/* <img src={img} alt="not found" /> */}
-        <h3>Ohh! page not found</h3>
-        <p>We can't seem to find the page you are looking for</p>
-        <Link to="/">back home</Link>
-      </div>
-    </ErrorStyles>
+    <section class="error-page-box">
+      <img class="error-page-img" src={img} alt="not found" />
+      <h3>Ohh! page not found</h3>
+      <p>We can't seem to find the page you are looking for</p>
+      <Link className="btn" to="/">
+        back home
+      </Link>
+    </section>
   )
 }
 export default Error
