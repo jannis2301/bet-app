b77a6ed080a88f6762b9bbb82ecff529403df92a
jannis2301 <jannis.ax@gmx.de>
2023-11-16 20:32:10 +0100

    refactor: update vite configs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-  const config = {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: process.env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: true,
+    proxy: {
+      '/api': {
+        target:
+          process.env.VITE_API_URL || 'https://bet-app-4w2c.onrender.com/',
+        changeOrigin: true,
       },
     },
-  }
-  return defineConfig(config)
-}
+  },
+  build: {
+    minify: true,
+  },
+})
